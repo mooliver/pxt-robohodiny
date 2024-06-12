cdfc032d4e6ab0b56409ca115c9c8f28c79935f5
Oliver M <147391014+mooliver@users.noreply.github.com>
2024-06-12 18:56:21 +0200

    Created function showing time

--- main.ts
+++ main.ts
@@ -2,11 +2,10 @@
 // P0 - malý ciferník (24 LED)
 // P1 - velký ciferník (60 LED)
 // P2 - stavové ledky (4 LED, dotyková tlačítka)
-// P13 - dotykové tlačítko L1 (l1)
-// P14 - dotykové tlačítko L2 (l2)
-// P15 - dotykové tlačítko R1 (r1)
-// P16 - dotykové tlačítko R2 (r2)
-
+// P13 - dotykové tlačítko L1 - led 3
+// P14 - dotykové tlačítko L2 - led 2
+// P15 - dotykové tlačítko R1 - led 0
+// P16 - dotykové tlačítko R2 - led 1
 
 // Led pasky
 const malyCif = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
@@ -25,10 +24,10 @@ function showLeds() { // Funkce zobrazí všechny LEDky
     stavLed.show()
 }
 
-function clearLeds() {
+function clearLeds() { // Funkce skryje všechny LEDky
     malyCif.clear(),
     velkyCif.clear(),
-    stavLed.clear()
+    showLeds()
 }
 
 function checkButtons() {
@@ -38,3 +37,38 @@ function checkButtons() {
     r2 = pins.digitalReadPin(DigitalPin.P16)
 }
 
+function hodinyCas() {
+    stavLed.clear()
+    stavLed.setPixelColor(3, neopixel.rgb(0, 200, 10))
+    loops.everyInterval(5000, function () {
+        clearLeds()
+        malyCif.setPixelColor((DS3231.hour() - 1), neopixel.rgb(0, 200, 10))
+        if (DS3231.minute() <= 30) {
+            velkyCif.setPixelColor((DS3231.minute() + 30), neopixel.rgb(0, 200, 10))
+        } else {
+            velkyCif.setPixelColor((DS3231.minute() - 30), neopixel.rgb(0, 200, 10))
+        }
+        showLeds()
+        console.log("spusteno")
+    })
+}
+
+function stopky() {
+    stavLed.clear()
+    stavLed.setPixelColor(2, neopixel.rgb(0, 200, 10))
+    loops.everyInterval(5000, function () {
+        console.log("Spusteno")
+    })
+}
+
+
+
+input.onButtonPressed(Button.A, function() {
+    DS3231.dateTime(2024, 6, 12, 3, 18, 53, 30)
+})
+
+input.onButtonPressed(Button.B, function() {
+    loops.everyInterval(5000, function() {
+        basic.showNumber(DS3231.second())
+    })
+})
